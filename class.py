60750c6578ac4b7acc293de8e6f0945e3e96297c
Oksana Feshchenko <foksanv@gmail.com>
2022-10-18 14:11:58 +0200

    class created

---
+++ class.py
@@ -0,0 +1,12 @@
+import random
+
+
+class Player:
+    def __init__(self, name, team, score):
+        self.name = name
+        self.team = team
+        self.score = score
+
+    def play(self):
+        num = random.randint(1, 11)
+        self.score += num
